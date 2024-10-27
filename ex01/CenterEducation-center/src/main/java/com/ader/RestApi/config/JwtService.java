a2859d0abacaf1eb4320324b27e97f3c16a1f82a
mamali543 <amalyreda@gmail.com>
2024-10-27 09:56:53 +0100

    Authentication filter ...

---
+++ ex01/CenterEducation-center/src/main/java/com/ader/RestApi/config/JwtService.java
@@ -0,0 +1,63 @@
+package com.ader.RestApi.config;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "4iSpEg/EnSthq/CkF5qk0x049yVUrCS5A1h+yCOPrqNi+MElXyKr2WitOA0KhrMS";
+    public String extractUsername(String jwt) {
+        return extractClaim(jwt, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String jwt, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(jwt);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String jwt) {
+        return Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJws(jwt).getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername()).setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+        .signWith(getSignInKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    public boolean isTokenValid(String jwt, UserDetails userDetails) {
+        final String username = extractUsername(jwt);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(jwt);
+    }
+
+    private boolean isTokenExpired(String jwt) {
+        return extractExpiration(jwt).before(new Date());
+    }
+
+    private Date extractExpiration(String jwt) {
+        return extractClaim(jwt, Claims::getExpiration);
+    }
+}
--- ex01/CenterEducation-center/src/test/java/com/ader/RestApi/RestApiApplicationTests.java
+++ ex01/CenterEducation-center/src/test/java/com/ader/RestApi/RestApiApplicationTests.java
@@ -1,16 +1,8 @@
 package com.ader.RestApi;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-
-import com.ader.RestApi.config.TestConfig;
-
 @SpringBootTest
-@Import(TestConfig.class)
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class RestApiApplicationTests {
 
 	@Test
