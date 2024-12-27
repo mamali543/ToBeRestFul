099411d763ab52b534598c8abeab761308401f10
mamali543 <amalyreda@gmail.com>
2024-12-27 05:31:06 +0100

    tomcat and postgres deployed

--- ex00/CenterEducation-center/src/main/java/com/ader/RestApi/RestApiApplication.java
+++ ex00/CenterEducation-center/src/main/java/com/ader/RestApi/RestApiApplication.java
@@ -2,13 +2,20 @@ package com.ader.RestApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Center Education API", version = "1.0", description = "API for Center Education"))
-public class RestApiApplication {
+public class RestApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
